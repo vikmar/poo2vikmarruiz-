1af22cfd34b216fb74d3f7c77200dd655a197de0
Administrador <Administrador@lds18.unac.edu.co>
2014-03-12 16:22:18 -0500

    Guardando Cambieos en el repositorio

---
+++ src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1;
+
+/**
+ *
+ * @author VikmarSair
+ */
+public class Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Hipotenusa resultado = new Hipotenusa();
+        resultado.setVisible(true);
+    }
+}
